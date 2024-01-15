f347fe9fc5f94e1164067db41775e392392ec841
정채운 <toyboy01113@gmail.com>
2024-01-15 22:33:11 +0900

    [Refactor] Photon Refactoring
    포톤 리펙트링

--- HIGHFIVE/Assets/Scripts/Managers/Main.cs
+++ HIGHFIVE/Assets/Scripts/Managers/Main.cs
@@ -9,6 +9,8 @@ public class Main : MonoBehaviour
     private SceneManagerEx _sceneManagerEx = new SceneManagerEx();
     private NetworkManager _networkManager = new NetworkManager();
     private DataManager _dataManager = new DataManager();
+    private ResourceManager _resourceManager = new ResourceManager();
+    public static ResourceManager ResourceManager { get { return Instance._resourceManager; } }
     public static DataManager DataManager { get { return Instance._dataManager; } }
     public static SceneManagerEx SceneManagerEx { get { return Instance._sceneManagerEx; } }
     public static NetworkManager NetworkManager { get { return Instance._networkManager; } } // set안써주면 알아서 private(아예 접근이 안됌)
