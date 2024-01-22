9bb275f9bc9c598c3751fa835c3c89ea7466115b
yskybright <rgh99@naver.com>
2024-01-22 21:18:06 +0900

    [Mod] Build Setting Scenes

--- HIGHFIVE/Assets/Scripts/Test/Test_RoundTimer.cs
+++ HIGHFIVE/Assets/Scripts/Test/Test_RoundTimer.cs
@@ -5,39 +5,39 @@ using UnityEngine.UI;
 using TMPro;
 using System;
 
-public class SelectScene_Timer : MonoBehaviour
+public class Test_RoundTimer : MonoBehaviour
 {
-    [SerializeField] private TMP_Text text;
+    //[SerializeField] private TMP_Text text;
 
-    [SerializeField] private float time;
-    [SerializeField] private float curTime;
+    //[SerializeField] private float time;
+    //[SerializeField] private float curTime;
 
-    // int minute;
-    int second;
+    //// int minute;
+    //int second;
 
-    private void Awake()
-    {
-        time = 5;
-        StartCoroutine(StartTimer());
-    }
+    //private void Awake()
+    //{
+    //    time = 5;
+    //    StartCoroutine(StartTimer());
+    //}
 
-    IEnumerator StartTimer()
-    {
-        curTime = time;
-        while (curTime > 0)
-        {
-            curTime -= Time.deltaTime;
-            // minute = (int)curTime / 60;
-            second = (int)curTime % 60;
-            text.text = second.ToString("0");
-            yield return null;
+    //IEnumerator StartTimer()
+    //{
+    //    curTime = time;
+    //    while (curTime > 0)
+    //    {
+    //        curTime -= Time.deltaTime;
+    //        // minute = (int)curTime / 60;
+    //        second = (int)curTime % 60;
+    //        text.text = second.ToString("0");
+    //        yield return null;
 
-            if (curTime <= 0)
-            {
-                Main.SceneManagerEx.LoadScene(Define.Scene.GameScene);
-                curTime = 0;
-                yield break;
-            }
-        }
-    }
+    //        if (curTime <= 0)
+    //        {
+    //            Main.SceneManagerEx.LoadScene(Define.Scene.GameScene);
+    //            curTime = 0;
+    //            yield break;
+    //        }
+    //    }
+    //}
 }
