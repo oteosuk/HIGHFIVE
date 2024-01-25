2f79947cfb455657b05b9215435dd6a56cd722a9
정채운 <toyboy01113@gmail.com>
2024-01-25 19:43:16 +0900

    [Fix] Exel Conflict
    엑셀 충돌 해결

--- HIGHFIVE/Assets/Scripts/Object/Monster/Monster.cs
+++ HIGHFIVE/Assets/Scripts/Object/Monster/Monster.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
+    public Stat stat;
     protected MonsterStateMachine _monsterStateMachine;
+    public Vector2 _spawnPoint;
+
     public Rigidbody2D Rigidbody { get; protected set; }
     public PlayerInput Input { get; protected set; }
     public Collider2D Controller { get; set; }
     public Animator Animator { get; set; }
-    public MonsterAnimationData _monsterAnimationData { get; set; }
+    public MonsterAnimationData MonsterAnimationData { get; set; }
 
 
     protected virtual void Awake()
@@ -19,13 +22,16 @@ public class Monster : MonoBehaviour
         Controller = GetComponent<Collider2D>();
         Animator = GetComponent<Animator>();
 
-        _monsterAnimationData = new MonsterAnimationData();
+        stat = GetComponent<Stat>();
+        SetSpawnPoint(transform.position);
+
+        MonsterAnimationData = new MonsterAnimationData();
     }
 
     protected virtual void Start()
     {
         _monsterStateMachine = new MonsterStateMachine(this);
-        _monsterAnimationData.Initialize();
+        MonsterAnimationData.Initialize();
         _monsterStateMachine.ChangeState(_monsterStateMachine._monsterIdleState);
     }
 
@@ -37,6 +43,9 @@ public class Monster : MonoBehaviour
     protected virtual void FixedUpdate()
     {
         _monsterStateMachine.PhysicsUpdate();
-
+    }
+    private void SetSpawnPoint(Vector2 spawnPoint)
+    {
+        _spawnPoint = spawnPoint;
     }
 }
