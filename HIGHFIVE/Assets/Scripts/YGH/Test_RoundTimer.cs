7111855aa752263d1a5aa1a9ade669be461c1aaf
yskybright <rgh99@naver.com>
2024-01-26 19:13:00 +0900

    [Feat] GameSceneController
    - 라운도 로직을 위해 필요한 이벤트를 담는 컨트롤러

--- HIGHFIVE/Assets/Scripts/YGH/Test_RoundTimer.cs
+++ HIGHFIVE/Assets/Scripts/YGH/Test_RoundTimer.cs
@@ -29,18 +29,18 @@ public class Test_RoundTimer : MonoBehaviour
 
     IEnumerator StartTimer()
     {
-        yield return new WaitForSeconds(3.0f); // 화면 전환을 위해 잠깐 기다림
+        yield return new WaitForSeconds(1.0f); // 화면 전환을 위해 잠깐 기다림
         yield return StartCoroutine(FarmingTimer());
 
         if (curTime == battleTime)
         {
-            yield return new WaitForSeconds(3.0f); // 화면 전환을 위해 잠깐 기다림
+            yield return new WaitForSeconds(1.0f); // 화면 전환을 위해 잠깐 기다림
             yield return StartCoroutine(BattleTimer());
         }
 
         if (!(TeamAScore != 1) || !(TeamBScore != 1))
         {
-            yield return new WaitForSeconds(3.0f); // 화면 전환을 위해 잠깐 기다림
+            yield return new WaitForSeconds(1.0f); // 화면 전환을 위해 잠깐 기다림
             yield return StartCoroutine(FarmingTimer());
         }
 
@@ -73,7 +73,7 @@ public class Test_RoundTimer : MonoBehaviour
 
     IEnumerator BattleTimer()
     {
-        battleTime = 7;
+        battleTime = 5;
         curTime = battleTime;
         while (curTime > 0)
         {
