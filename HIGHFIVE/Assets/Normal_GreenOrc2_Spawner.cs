1f3839639d6555450698aee67c1292cfaf2a00aa
oteosuk <oteosuk@naver.com>
2024-02-13 18:58:41 +0900

    [Mod] Spawner Fix

---
+++ HIGHFIVE/Assets/Normal_GreenOrc2_Spawner.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Normal_GreenOrc2_Spawner : MonsterSpawner
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        CheckFull();
+        if (!isFull)
+        {
+            _curTime -= Time.deltaTime;
+            if (_curTime <= 0)
+            {
+                ReSpawn("Monster/Normal/GreenOrc2");
+            }
+        }
+    }
+}
