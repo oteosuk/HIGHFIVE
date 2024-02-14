f16a26a2e76d2f733e7ca8d55d6c4d7478752b35
sasay7783 <sasay7783@nate.com>
2024-02-14 21:34:47 +0900

    [FIx] Loading or SkillInfoPanel

--- HIGHFIVE/Assets/Scripts/UI/Scene_UI/SkillExplan_UI.cs
+++ HIGHFIVE/Assets/Scripts/UI/Scene_UI/SkillExplan_UI.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class SkillExplan_UI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public GameObject _explain;
+    public GameObject _info;
+    public TMP_Text _skillName;
+    public TMP_Text _skillInfo;
+
+
     public void OnPointerEnter(PointerEventData eventData)
     {
-        _explain.SetActive(true);
+        Character myCharacter = Main.GameManager.SpawnedCharacter;
+        _info.SetActive(true);
+        Debug.Log(myCharacter.CharacterSkill.FirstSkill.skillData.skillName);
+        _skillName.text = myCharacter.CharacterSkill.FirstSkill.skillData.skillName;
+        _skillInfo.text = myCharacter.CharacterSkill.FirstSkill.skillData.info;
+        
     }
-
     public void OnPointerExit(PointerEventData eventData)
     {
-        _explain.SetActive(false);
+        _info.SetActive(false);
     }
 
 }
--- HIGHFIVE/Assets/Scripts/Utils/LoadingAnimation.cs
+++ HIGHFIVE/Assets/Scripts/Utils/LoadingAnimation.cs
@@ -24,14 +24,21 @@ public class LoadingAnimation : MonoBehaviour
         // 몬스터 프리펩
         Main.ResourceManager.Load<GameObject>("Prefabs/Monster/Tree");
         Debug.Log("Monster Load!!");
+
+        // UI
+        Main.ResourceManager.Load<GameObject>("Prefabs/UI_Prefabs/GameSceneUI");
+        Debug.Log("UI Load!!");
     }
 
     IEnumerator LoadSceneProcess()
     {
+        //게임 씬을 로드, 진행도를 확인
         AsyncOperation op = SceneManager.LoadSceneAsync((int)Define.Scene.GameScene);
-        op.allowSceneActivation = false;
+        op.allowSceneActivation = false;    
+
         while (!op.isDone)
         {
+            // 정규화 - 0부터 1사이 값으로 만든다.
             float progress = Mathf.Clamp01(op.progress / 0.9f);
             loadingBar.fillAmount = progress;
 
@@ -39,10 +46,9 @@ public class LoadingAnimation : MonoBehaviour
             {
                 LoadFile();
                 yield return new WaitForSeconds(1f);
+                // 씬을 활성화
                 op.allowSceneActivation = true;
             }
-
-
             yield return null;
         }
     }
